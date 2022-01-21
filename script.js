7ba08b2a7d75cf96db01e9944d0774c0dcbf1907
amrita-padhy <amrita.padhy5@gmail.com>
2022-01-21 15:27:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+import calaulator from "./calculator.js"
+
+
+
+
+/* ACTIONS
+1-check a number
+2-click clear button
+3-click delet button
+4-click an operation
+5-click the period button
+6-click equals
+*/
